49ca210b262baf2e712ef02a42e7c008088babe6
Nicholas Faz <nicholasfaz@gmail.com>
2025-05-05 19:45:00 -0400

    UI updated, added images, added cart

--- catalog/models.py
+++ catalog/models.py
@@ -4,3 +4,10 @@ class Product(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(upload_to='products/', blank=True)
+    quantity = models.IntegerField()
+
+class Cart(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
